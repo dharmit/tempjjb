3fb0d844bed04f2738f9833e8f8f2cbea6fff341
Dharmit Shah <shahdharmit@gmail.com>
2016-03-15 15:59:43 +0530

    git diff

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
-print "JJB Example"
+#!/usr/bin/python
+
+import os
+import subprocess
+
+subprocess.check_call(["git", "diff-tree", "--no-commit-id", "--name-only",
+                       "-r", "%s" % os.environ['ghprbActualCommit']])
